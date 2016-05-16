36a08a38dfd9fa6c751ab1e0939ae41b9d729756
apspecial <guoke8181@sina.com>
2016-05-16 16:51:03 +1000

    Add files via upload

---
+++ StaySteady.Mobile/StaySteady.Mobile/Views/ReportPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StaySteady.Mobile
+{
+	public partial class ReportPage : ContentPage
+	{
+		public ReportPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
