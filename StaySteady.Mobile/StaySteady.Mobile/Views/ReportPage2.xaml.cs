9871b287bcd4f44da1b6d21e640cac1ea565141a
david.acero@outlook.com <david.acero@outlook.com>
2016-05-18 11:04:30 +1000

    Fixed build

---
+++ StaySteady.Mobile/StaySteady.Mobile/Views/ReportPage2.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace StaySteady.Mobile.Views
+{
+    public partial class ReportPage2 : ContentPage
+    {
+        public ReportPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StaySteady.Mobile/StaySteady.Mobile/Views/SummaryPage2.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace StaySteady.Mobile.Views
+{
+    public partial class SummaryPage2 : ContentPage
+    {
+        public SummaryPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
