bfa66a9e109552d7fc4f77209692be4c518f1a8e
Juan Camilo Ibañez Torres <juanc5ibanez@hotmail.com>
2016-05-24 22:27:33 +1000

    Add push notifications to droid

--- StaySteady.Mobile/StaySteady.Mobile.Droid/MainActivity.cs
+++ StaySteady.Mobile/StaySteady.Mobile.Droid/MainActivity.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using StaySteady.Mobile.Droid.Utility;
 using StaySteady.Mobile.Utility;
 using Xamarin.Forms;
+using Gcm.Client;
 
 namespace StaySteady.Mobile.Droid
 {
@@ -17,11 +18,52 @@ namespace StaySteady.Mobile.Droid
     {
         protected override void OnCreate(Bundle bundle)
         {
+            // Set the current instance of MainActivity.
+            instance = this;
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+            try
+            {
+                // Check to ensure everything's setup right
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+
+                // Register for push notifications
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the Mobile Service. Verify the URL", "Error");
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e.Message, "Error");
+            }
             DependencyService.Register<IDialer,PhoneDialer>();
+        }
+
+        private void CreateAndShowDialog(String message, String title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
+        }
+
+        // Create a new instance field for this activity.
+        static MainActivity instance = null;
+
+        // Return the current activity instance.
+        public static MainActivity CurrentActivity
+        {
+            get
+            {
+                return instance;
+            }
         }
     }
 }
