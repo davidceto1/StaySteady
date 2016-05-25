88e34d9642cee45b1a30ea9f770d07d50d65efe1
sildejyt <sildejyt@hotmail.com>
2016-05-25 10:28:11 +1000

    Added Tutorial

---
+++ StaySteady.Mobile/StaySteady.Mobile/Views/Exercise1Xaml.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StaySteady.Mobile
+{
+	public partial class Exercise1Xaml : ContentPage
+	{
+		public Exercise1Xaml ()
+		{
+			Title = "Exercise 1";
+			Icon = "icon.png";
+			InitializeComponent ();
+		}
+	}
+}
+
---
+++ StaySteady.Mobile/StaySteady.Mobile/Views/Exercise2Xaml.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StaySteady.Mobile
+{
+	public partial class Exercise2Xaml : ContentPage
+	{
+		public Exercise2Xaml ()
+		{
+			Title = "Exercise 2";
+			Icon = "icon.png";
+			InitializeComponent ();
+		}
+	}
+}
+
