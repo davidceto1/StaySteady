f9cda17de35f1c02ce2c0f969d0912b65bdf87ed
dari <darina.ganeva@yahoo.com>
2016-05-26 13:32:44 +1000

    Convert DailyActivity to MVVM

--- StaySteady.Mobile/StaySteady.Mobile/Views/DailyActivity.xaml.cs
+++ StaySteady.Mobile/StaySteady.Mobile/Views/DailyActivity.xaml.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using StaySteady.Mobile.Models;
+using StaySteady.Mobile.Utility;
 using Xamarin.Forms;
+using StaySteady.Mobile.ViewModels;
+using GalaSoft.MvvmLight.Views;
 
 namespace StaySteady.Mobile.Views
 {
     public partial class DailyActivity : ContentPage
     {
+        private DailyActivityViewModel _viewModel;
+
         public DailyActivity()
         {
             InitializeComponent();
+            _viewModel = new DailyActivityViewModel(DependencyService.Get<INavigationService>());
+            this.BindingContext = _viewModel;           
         }
     }
 }
