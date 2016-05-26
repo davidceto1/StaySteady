c362da1885505d0e3ca5f224738fd235abd5a94e
Juan Camilo Ibañez Torres <juanc5ibanez@hotmail.com>
2016-05-26 10:49:03 +1000

    Add SQLite support

---
+++ StaySteady.Mobile/StaySteady.Mobile.iOS/Utility/SQLite_iOS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using StaySteady.Mobile.iOS.Utility;
+using StaySteady.Mobile.Utility;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_iOS))]
+namespace StaySteady.Mobile.iOS.Utility
+{
+    public class SQLite_iOS : ISQLite
+    {
+        public SQLite_iOS() { }
+        public SQLite.SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "TodoSQLite.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            string libraryPath = Path.Combine(documentsPath, "..", "Library"); // Library folder
+            var path = Path.Combine(libraryPath, sqliteFilename);
+            // Create the connection
+            var conn = new SQLite.SQLiteConnection(path);
+            // Return the database connection
+            return conn;
+        }
+    }
+}
